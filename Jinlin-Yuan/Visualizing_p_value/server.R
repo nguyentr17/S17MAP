3e316516d8df210a7643b03c6d1614f25c41ee47
gdomnijl <gdomnijl@gmail.com>
2017-03-07 00:47:44 -0600

    visualizing p-value three basic reactive plots

--- Jinlin-Yuan/Visualizing_p_value/server.R
+++ Jinlin-Yuan/Visualizing_p_value/server.R
@@ -1,53 +1,91 @@
 function(input, output) {
- 
-  output$distPlot <- renderPlot({
-    par(mfrow = c(2,1))
-    
+   hx1 <- reactive({
+     switch(input$population1,
+            "normal" = {rnorm(500, input$popu_mean1, input$sd1)},
+            "skewed" = {rchisq(500, df=4) + input$popu_mean1},
+            "uniform" = {runif(500, min = input$popu_mean1 - input$sd1, max = input$popu_mean1 + input$sd1)})
+   })
+   hx2 <- reactive({
+     switch(input$population2,
+            "normal" = {rnorm(500, input$popu_mean2, input$sd2)},
+            "skewed" = {rchisq(500, df=4) + input$popu_mean2},
+            "uniform" = {runif(500, min = input$popu_mean2 - input$sd2, max = input$popu_mean2 + input$sd2)})
+   })
+  
+  
+  output$distPlot1 <- renderPlot({
+    ## made change 1 for dynamic plotting
+    low_range = min(input$popu_mean1-4*input$sd1, input$popu_mean2-4*input$sd2)
+    high_range = max(input$popu_mean1+4*input$sd1, input$popu_mean2+4*input$sd2)
+    height = 0.4/input$sd1
+
     switch(input$population1,
            "normal" = {
-             hx1 <- rnorm(input$size1, input$mean1, input$sd1)
-             width = input$mean1 + 4*input$sd1
-             hist(hx1, prob = TRUE, main="Normal", cex.axis=.8, xlim=c(-width,width), breaks=5)
-             curve(dnorm(x, mean = input$mean1, sd = input$sd1), 
-                   col="darkblue", lwd=2, add=TRUE, yaxt="n")
-             },
+          ##   hx1 <- rnorm(500, input$popu_mean1, input$sd1)
+             hist(hx1(), prob = TRUE, main="Normal", cex.axis=.8, 
+                  ## made changes 2
+                  xlim=c(low_range,high_range),
+                  ylim = c(0,height), 
+                  breaks=5)
+             curve(dnorm(x, mean = input$popu_mean1, sd = input$sd1), 
+                   col="red", lwd=2, add=TRUE, yaxt="n")
+           },
            "skewed" = {
-             hx1 <- rchisq(input$size1, df=4) + input$mean1
-             width = input$mean1 + 4*input$sd1
-             hist(hx1, prob = TRUE, main="Skewed", cex.axis=.8, xlim=c(-width,width), breaks=5)
+             ##hx1 <- rchisq(500, df=4) + input$popu_mean1
+             hist(hx1(), prob = TRUE, main="Skewed", cex.axis=.8, xlim=c(low_range,high_range), ylim = c(0,height),breaks=5)
              curve(dchisq(x, df = 4), 
-                   col="darkblue", lwd=2, add=TRUE, yaxt="n")     
+                   col="red", lwd=2, add=TRUE, yaxt="n")     
            },
            "uniform" =  {
-             hx1 <- runif(input$size1, min = input$mean1 - input$sd1, max = input$mean1 + input$sd1)
-             
-             hist(hx1, prob = TRUE, main="Uniform", cex.axis=.8, xlim=c(-3,3), breaks=5)
-             curve(dunif(x, min = input$mean1 - input$sd1, max = input$mean1 + input$sd1), 
-                   col="darkblue", lwd=2, add=TRUE, yaxt="n")
-           })
-    
-    switch(input$population2,
-           "normal" = {
-             hx2 <- rnorm(input$size2, input$mean2, input$sd2)
-             width = input$mean2 + 4*input$sd2
-             hist(hx2, prob = TRUE, main="Normal", cex.axis=.8, xlim=c(-width,width), breaks=5)
-             curve(dnorm(x, mean = input$mean2, sd = input$sd2), 
-                   col="darkblue", lwd=2, add=TRUE, yaxt="n")},
-     
-           "skewed" = {
-             hx2 <- rchisq(input$size2, df=4) + input$mean2
-             width = input$mean2 + 4*input$sd2
-             hist(hx2, prob = TRUE, main="Skewed", cex.axis=.8, xlim=c(-width,width), breaks=5)
-             curve(dchisq(x, df = 4), 
-                   col="darkblue", lwd=2, add=TRUE, yaxt="n")}, 
+             ##hx1 <- runif(500, min = input$popu_mean1 - input$sd1, max = input$popu_mean1 + input$sd1)
+             hist(hx1(), prob = TRUE, main="Uniform", cex.axis=.8, xlim=c(-3,3), breaks=5)
+             curve(dunif(x, min = input$popu_mean1 - input$sd1, max = input$popu_mean1 + input$sd1), 
+                   col="red", lwd=2, add=TRUE, yaxt="n")
+           }
            
-           "uniform" = {
-             hx2 <- runif(input$size2, min = input$mean2 - input$sd2, max = input$mean2 + input$sd2)
-             
-             hist(hx2, prob = TRUE, main="Uniform", cex.axis=.8, xlim=c(-3,3), breaks=5)
-             curve(dunif(x, min = input$mean2 - input$sd2, max = input$mean2 + input$sd2), 
-                   col="darkblue", lwd=2, add=TRUE, yaxt="n")}
     )
+    output$distPlot2 <- renderPlot({
+      half_width =  4*input$sd1
+      height = 0.4/input$sd1
+      switch(input$population2,
+             "normal" = {
+            ##   hx2 <- rnorm(500, input$popu_mean2, input$sd2)
+               half_width = input$popu_mean2 + 4*input$sd2
+               height = 0.4/input$sd2
+               hist(hx2(), prob = TRUE, main="Normal", cex.axis=.8, xlim=c(low_range,high_range),ylim = c(0,height), breaks=5)
+               curve(dnorm(x, mean = input$popu_mean2, sd = input$sd2), 
+                     col="blue", lwd=2, add=TRUE, yaxt="n")},
+             
+             "skewed" = {
+            ##   hx2 <- rchisq(500, df=4) + input$popu_mean2
+               half_width = input$popu_mean2 + 4*input$sd2
+               height = 0.4/input$sd2
+               hist(hx2(), prob = TRUE, main="Skewed", cex.axis=.8, xlim=c(low_range,high_range), ylim = c(0,height),breaks=5)
+               curve(dchisq(x, df = 4), 
+                     col="blue", lwd=2, add=TRUE, yaxt="n")}, 
+             
+             "uniform" = {
+              ## hx2 <- runif(500, min = input$popu_mean2 - input$sd2, max = input$popu_mean2 + input$sd2)
+               hist(hx2(), prob = TRUE, main="Uniform", cex.axis=.8, xlim=c(-3,3), breaks=5)
+               curve(dunif(x, min = input$popu_mean2 - input$sd2, max = input$popu_mean2 + input$sd2), 
+                     col="blue", lwd=2, add=TRUE, yaxt="n")}
+      )
+    })
+    
+    output$distPlot3 <- renderPlot({
+      sp1 <- sample(hx1(), input$size1, replace = TRUE, prob = NULL)
+      sp2 <- sample(hx2(), input$size2, replace = TRUE, prob = NULL)
+      # first plot
+      plot(sp1, seq_along(sp1), xlim=range(c(sp1,sp2)), col = 'red', pch = 16, cex = 1.5)
+      # second plot  EDIT: needs to have same ylim
+      par(new = TRUE)
+      plot(sp2, seq_along(sp2),xlim=range(c(sp1,sp2)), axes = FALSE, xlab = "", ylab = "", col = 'blue', pch = 17, cex = 1.5)
+    })
+    
+   
+   ## output$test_stats <- renderDataTable(expr, options = NULL, searchDelay = 500,
+     ##               callback = "function(oTable) {}", escape = TRUE, env = parent.frame(),
+       ##             quoted = FALSE, outputArgs = list())
   })
   
   
@@ -60,8 +98,7 @@ function(input, output) {
   
   # Q: overlay the two graphs or show in a column?
   
-  return(data)
-  
+
   #plot(hx1, col="red", type = "h", xlab = "", ylab = "",
   #     main = "normal", axes = FALSE)
   
--- Jinlin-Yuan/Visualizing_p_value/ui.R
+++ Jinlin-Yuan/Visualizing_p_value/ui.R
@@ -1,19 +1,20 @@
 fluidPage(
-  headerPanel("Visualizing sample mean difference"),
-    sidebarPanel(
+  headerPanel("Visualizing two-sample t-test"),
+  sidebarLayout(  
+  sidebarPanel(
       
       selectInput(inputId='population1', 
                   label = 'population1',
                   choice = c('normal','skewed','uniform')
                   ),
-      numericInput(inputId = 'mean1',
+      numericInput(inputId = 'popu_mean1',
                 label = 'Population mean', 
                 0,
                 min = -100, max = 100),
       
       numericInput(inputId = 'size1',
                 label = 'Sample size',
-                   100,
+                   20,
                 min = -100, max = 100),
       numericInput(inputId = 'sd1',
                    label = 'Standard diviation',
@@ -23,24 +24,28 @@ fluidPage(
                   label = 'population2',
                   choice = c('normal','skewed','uniform')
       ),
-      numericInput(inputId = 'mean2',
+      numericInput(inputId = 'popu_mean2',
                 label = 'Population mean', 
                 0,
                 min = -100, max = 100),
       
       numericInput(inputId = 'size2',
                 label = 'Sample size',
-                100,
+                20,
                 min = -100, max = 100),
       numericInput(inputId = 'sd2',
                 label = 'Standard diviation',
                 10,
-                min = -100, max = 100),
+                min = -100, max = 100)),
       
      
     
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("distPlot1"),
+      ## Made changes: plotted seperately
+      plotOutput("distPlot2"),
+      plotOutput("distPlot3")
+      
     )
     ))
 

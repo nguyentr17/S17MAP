42ccf67d70ed42999a6a5469cc71663e6a8dff7d
wangyuan17sp <wangyuan@grinnell.edu>
2017-03-13 20:50:35 -0500

    six plots oh yeah

--- Jinlin-Yuan/Func_3_dist_graph.R
+++ Jinlin-Yuan/Func_3_dist_graph.R
@@ -6,41 +6,15 @@ mean_norm_dist <- function(s, mean1, mean2, sd1, sd2, n1, n2) {
   #s <- readline()
   if (s == "normal") {
     hx1 <- rnorm(n1, mean1, sd1)
-
     hx2 <- rnorm(n2, mean2, sd2)
-    
-    #hist(hx1, prob = TRUE, main="Normal", cex.axis=.8, xlim=c(-5,5), breaks=20)
-    #curve(dnorm(x, mean = mean1, sd = sd1), 
-    #     col="darkblue", lwd=2, add=TRUE, yaxt="n")
-    
-    #  hist(hx2, prob = TRUE, main="Normal", cex.axis=.8, xlim=c(-5,5), breaks=20)
-    # curve(dnorm(x, mean = mean2, sd = sd2), 
-    #      col="darkblue", lwd=2, add=TRUE, yaxt="n")
     
   } else if (s == "skewed") {
     hx1 <- rchisq(n1, df=4) + mean1
     hx2 <- rchisq(n2, df=4) + mean2
     
-    #hist(hx1, prob = TRUE, main="Skewed", cex.axis=.8, xlim=c(0,12), breaks=20)
-    #curve(dchisq(x, df = 4), 
-    #     col="darkblue", lwd=2, add=TRUE, yaxt="n")
-    
-    #  hist(hx2, prob = TRUE, main="Skewed", cex.axis=.8, xlim=c(0,12), breaks=20)
-    #  curve(dchisq(x, df = 4), 
-    #       col="darkblue", lwd=2, add=TRUE, yaxt="n")
-    
   } else if (s == "uniform") {
     hx1 <- runif(n1, min = mean1 - sd1, max = mean1 + sd1)
     hx2 <- runif(n2, min = mean2 - sd2, max = mean2 + sd2)
-    
-  #  hist(hx1, prob = TRUE, main="Uniform", cex.axis=.8, xlim=c(-3,3), breaks=20)
-  #  curve(dunif(x, min = mean1 - sd1, max = mean1 + sd1), 
-  #        col="darkblue", lwd=2, add=TRUE, yaxt="n")
-    
-  #  hist(hx2, prob = TRUE, main="Uniform", cex.axis=.8, xlim=c(-3,3), breaks=20)
-  #  curve(dunif(x, min = mean2 - sd2, max = mean2 + sd2), 
-  #        col="darkblue", lwd=2, add=TRUE, yaxt="n")
-    
     
   } else {
     print("Distribution not found.")
@@ -53,30 +27,32 @@ mean_norm_dist <- function(s, mean1, mean2, sd1, sd2, n1, n2) {
   
   data <- c(mean_diff, tscore, pval)
   
-  
-  # Q: overlay the two graphs or show in a column?
-  
   return(data)
   
-  #plot(hx1, col="red", type = "h", xlab = "", ylab = "",
-  #     main = "normal", axes = FALSE)
-  
-  #plot(hx2, col="green", type = "h", xlab = "", ylab = "",
-  #     main = "normal", axes = FALSE)
   
 }
 
-library(mosaic)
 
 three_plots <- function(reps, dist, mean1, mean2, sd1, sd2, n1, n2) {
   data <- vector()
   for (i in 1:reps) {
     data <- cbind(data, mean_norm_dist(dist, mean1, mean2, sd1, sd2, n1, n2))
   }
-  par(mfrow = c(3,1))
-  hist(data[1,], col = "red", pch = 16, main = "Dotplot of Mean Difference", breaks = 30)
-  hist(data[2,], col = "blue", pch = 16, main = "Dotplot of t-statistics", breaks = 30)
-  hist(data[3,], col= "green", pch = 20, main = "Dotplot of p-values", breaks = 30)
+  par(mfrow = c(3,2))
+  hist(data[1,], col = "plum", pch = 16, main = "Dotplot of Mean Difference", breaks = 30)
+  abline(v = mean1 - mean2, lwd = 2, col = "red")
+  plot(data[1,], seq_along(data[1,]), col = "plum", pch = 16, main = "Dotplot of t-statistics")
+  abline(v = mean1 - mean2, lwd = 2, col = "red")
+  
+  hist(data[2,], col = "wheat1", pch = 16, main = "Dotplot of t-statistics", breaks = 30)
+  abline(v = (mean1 - mean2)/(sqrt(sd1*sd1/n1 + sd2*sd2/n2)), lwd = 2, col = "red")  
+  plot(data[2,], seq_along(data[2,]), col = "wheat1", pch = 16, main = "Dotplot of t-statistics")
+  abline(v = (mean1 - mean2)/(sqrt(sd1*sd1/n1 + sd2*sd2/n2)), lwd = 2, col = "red")
+  
+  hist(data[3,], col= "palegreen", pch = 20, main = "Dotplot of p-values", breaks = 30)
+  abline(v = 0.05, lwd = 2, col = "red")
+  plot(data[3,], seq_along(data[3,]), col= "palegreen", pch = 20, main = "Dotplot of p-values")
+  abline(v = 0.05, lwd = 2, col = "red")
   
   #plot(data[1,], col = "red", pch = 16, main = "Dotplot of t-statistics")
   #plot(data[2,], col = "blue", pch = 16, main = "Dotplot of t-statistics")

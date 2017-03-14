f7fbff9800e117c856e921feefb5e0d80e5de7b0
wangyuan17sp <wangyuan@grinnell.edu>
2017-03-13 21:54:28 -0500

    its working yoo

--- Jinlin-Yuan/Visualizing_p_value/ui.R
+++ Jinlin-Yuan/Visualizing_p_value/ui.R
@@ -2,6 +2,8 @@ fluidPage(
   headerPanel("Visualizing two-sample t-test"),
   sidebarLayout(  
   sidebarPanel(
+    
+      width = 3,
       
       selectInput(inputId='population1', 
                   label = 'population1',
@@ -37,21 +39,23 @@ fluidPage(
       numericInput(inputId = 'sd2',
                 label = 'Standard diviation',
                 10,
-                min = -100, max = 100)),
+                min = -100, max = 100),
       selectInput(inputId='rep', 
               label = 'Number of Samples',
-              choice = c(100,200,500,1000,2000,5000))
-  ),
+              choice = c(100,200,500,1000,2000,5000))),
      
     
     mainPanel(
       plotOutput("distPlot1"),
       ## Made changes: plotted seperately
-      plotOutput("mean_diff"),
-      plotOutput("distPlot3"),
+      plotOutput("mean_diff1"),
+      plotOutput("mean_diff2"),
+      plotOutput("mean_diff3")
+   #   plotOutput("distPlot3"),
      # verbatimTextOutput("test_stats"),
-     textOutput("test")
+  #   textOutput("test")
     )
-    )
+  
+    ))
 
 

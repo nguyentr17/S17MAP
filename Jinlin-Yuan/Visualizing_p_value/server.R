2cf37493437cb65a391ed38961b524ebae660455
wangyuan17sp <wangyuan@grinnell.edu>
2017-03-13 22:27:18 -0500

    indentation

--- Jinlin-Yuan/Visualizing_p_value/server.R
+++ Jinlin-Yuan/Visualizing_p_value/server.R
@@ -1,16 +1,16 @@
 function(input, output) {
-   hx1 <- reactive({
-     switch(input$population1,
-            "normal" = {rnorm(500, input$popu_mean1, input$sd1)}, 
-            "skewed" = {rchisq(500, df=4) + input$popu_mean1},
-            "uniform" = {runif(500, min = input$popu_mean1 - input$sd1, max = input$popu_mean1 + input$sd1)})
-   })
-   hx2 <- reactive({
-     switch(input$population2,
-            "normal" = {rnorm(500, input$popu_mean2, input$sd2)},
-            "skewed" = {rchisq(500, df=4) + input$popu_mean2},
-            "uniform" = {runif(500, min = input$popu_mean2 - input$sd2, max = input$popu_mean2 + input$sd2)})
-   })
+  hx1 <- reactive({
+    switch(input$population1,
+           "normal" = {rnorm(500, input$popu_mean1, input$sd1)}, 
+           "skewed" = {rchisq(500, df=4) + input$popu_mean1},
+           "uniform" = {runif(500, min = input$popu_mean1 - input$sd1, max = input$popu_mean1 + input$sd1)})
+  })
+  hx2 <- reactive({
+    switch(input$population2,
+           "normal" = {rnorm(500, input$popu_mean2, input$sd2)},
+           "skewed" = {rchisq(500, df=4) + input$popu_mean2},
+           "uniform" = {runif(500, min = input$popu_mean2 - input$sd2, max = input$popu_mean2 + input$sd2)})
+  })
   
   output$distPlot1 <- renderPlot({
     ## made change 1 for dynamic plotting
@@ -25,13 +25,13 @@ function(input, output) {
                    xlim=c(low_range,high_range),
                    ylim = c(0,height), 
                    col="red",  lwd=2,  yaxt="n")
-                   abline(v = 0)},
-          
+             abline(v = 0)},
+           
            "skewed" = {
              curve(dchisq(x, df = 4), 
                    xlim=c(low_range,high_range), ylim = c(0,height),
                    col="red", lwd=2,  yaxt="n")},
-          
+           
            "uniform" = {
              ##hx1 <- runif(500, min = input$popu_mean1 - input$sd1, max = input$popu_mean1 + input$sd1)
              curve(dunif(x, min = input$popu_mean1 - input$sd1, max = input$popu_mean1 + input$sd1),
@@ -57,8 +57,8 @@ function(input, output) {
              ## hx2 <- runif(500, min = input$popu_mean2 - input$sd2, max = input$popu_mean2 + input$sd2)
              curve(dunif(x, min = input$popu_mean2 - input$sd2, max = input$popu_mean2 + input$sd2), 
                    xlim=c(-3,3), col="blue", lwd=2,  yaxt="n")})
-})
-
+  })
+  
   data <- reactive({
     d <- vector()
     for (i in 1:input$rep) {
@@ -72,60 +72,60 @@ function(input, output) {
     }
     return(d)
   })
-    # output$test <- renderPrint(data())
-    output$mean_diff1 <- renderPlot({
-      par(mfrow = c(1,2))
-      data <- vector()
-      data <- cbind(data, data())
-      
-      hist(data[1,], col = "plum", pch = 16, xlab = "", main = "Histogram of Mean Difference", breaks = 30)
-      abline(v = input$popu_mean1 - input$popu_mean2, lwd = 2, col = "red")
-      plot(data[1,], seq_along(data[1,]), col = "plum", pch = 16, xlab = "", main = "Scatterplot of t-statistics")
-      abline(v = input$popu_mean1 - input$popu_mean2, lwd = 2, col = "red")
-      })
+  # output$test <- renderPrint(data())
+  output$mean_diff1 <- renderPlot({
+    par(mfrow = c(1,2))
+    data <- vector()
+    data <- cbind(data, data())
     
-    output$mean_diff2 <- renderPlot({
-      par(mfrow = c(1,2))
-      data <- vector()
-      data <- cbind(data, data())
-      
-      hist(data[2,], col = "wheat1", pch = 16, xlab = "", main = "Histogram of t-statistics", breaks = 30)
-      abline(v = (input$popu_mean1 - input$popu_mean2)/(sqrt(input$sd1 * input$sd1/input$size1 + input$sd2 * input$sd2/input$size2)), lwd = 2, col = "red")  
-      plot(data[2,], seq_along(data[2,]), col = "wheat1", pch = 16, xlab = "", main = "Scatterplot of t-statistics")
-      abline(v = (input$popu_mean1 - input$popu_mean2)/(sqrt(input$sd1 * input$sd1/input$size1 + input$sd2 * input$sd2/input$size2)), lwd = 2, col = "red")
-    })
+    hist(data[1,], col = "plum", pch = 16, xlab = "", main = "Histogram of Mean Difference", breaks = 30)
+    abline(v = input$popu_mean1 - input$popu_mean2, lwd = 2, col = "red")
+    plot(data[1,], seq_along(data[1,]), col = "plum", pch = 16, xlab = "", main = "Scatterplot of t-statistics")
+    abline(v = input$popu_mean1 - input$popu_mean2, lwd = 2, col = "red")
+  })
+  
+  output$mean_diff2 <- renderPlot({
+    par(mfrow = c(1,2))
+    data <- vector()
+    data <- cbind(data, data())
     
-    output$mean_diff3 <- renderPlot({
-      par(mfrow = c(1,2))
-      data <- vector()
-      data <- cbind(data, data())
-      
-      hist(data[3,], col= "palegreen", pch = 20, xlab = "", main = "Histogram of p-values", breaks = 30)
-      abline(v = 0.05, lwd = 2, col = "red")
-      plot(data[3,], seq_along(data[3,]), col= "palegreen", pch = 20, xlab = "", main = "Scatterplot of p-values")
-      abline(v = 0.05, lwd = 2, col = "red")
-    })
-
-    #plot(data[1,], col = "red", pch = 16, main = "Dotplot of t-statistics")
-    #plot(data[2,], col = "blue", pch = 16, main = "Dotplot of t-statistics")
-    #plot(data[3,], col= "green", pch = 20, main = "Dotplot of p-values")
-    #return(data)
+    hist(data[2,], col = "wheat1", pch = 16, xlab = "", main = "Histogram of t-statistics", breaks = 30)
+    abline(v = (input$popu_mean1 - input$popu_mean2)/(sqrt(input$sd1 * input$sd1/input$size1 + input$sd2 * input$sd2/input$size2)), lwd = 2, col = "red")  
+    plot(data[2,], seq_along(data[2,]), col = "wheat1", pch = 16, xlab = "", main = "Scatterplot of t-statistics")
+    abline(v = (input$popu_mean1 - input$popu_mean2)/(sqrt(input$sd1 * input$sd1/input$size1 + input$sd2 * input$sd2/input$size2)), lwd = 2, col = "red")
+  })
+  
+  output$mean_diff3 <- renderPlot({
+    par(mfrow = c(1,2))
+    data <- vector()
+    data <- cbind(data, data())
+    
+    hist(data[3,], col= "palegreen", pch = 20, xlab = "", main = "Histogram of p-values", breaks = 30)
+    abline(v = 0.05, lwd = 2, col = "red")
+    plot(data[3,], seq_along(data[3,]), col= "palegreen", pch = 20, xlab = "", main = "Scatterplot of p-values")
+    abline(v = 0.05, lwd = 2, col = "red")
+  })
+  
+  #plot(data[1,], col = "red", pch = 16, main = "Dotplot of t-statistics")
+  #plot(data[2,], col = "blue", pch = 16, main = "Dotplot of t-statistics")
+  #plot(data[3,], col= "green", pch = 20, main = "Dotplot of p-values")
+  #return(data)
   
   
-   
-    
-    #sp1 <- reactive({sample(hx1(), input$size1, replace = TRUE, prob = NULL)})
-    #sp2 <- reactive({sample(hx2(), input$size2, replace = TRUE, prob = NULL)})
-    #output$distPlot3 <- renderPlot({
   
-      # Try histogram 
-     # hist(sp1(), col = )
-      #par(new = TRUE)
-     # hist(sp2())
-      ## Original 
-      # first plot
-   #plot(seq_along(sp1()),sp1(), ylim=range(c(sp1(),sp2())), col = 'red', pch = 16, cex = 1.5)
-      # second plot  EDIT: needs to have same ylim
+  
+  #sp1 <- reactive({sample(hx1(), input$size1, replace = TRUE, prob = NULL)})
+  #sp2 <- reactive({sample(hx2(), input$size2, replace = TRUE, prob = NULL)})
+  #output$distPlot3 <- renderPlot({
+  
+  # Try histogram 
+  # hist(sp1(), col = )
+  #par(new = TRUE)
+  # hist(sp2())
+  ## Original 
+  # first plot
+  #plot(seq_along(sp1()),sp1(), ylim=range(c(sp1(),sp2())), col = 'red', pch = 16, cex = 1.5)
+  # second plot  EDIT: needs to have same ylim
   #    par(new = TRUE)
   #   plot(seq_along(sp2()),sp2(),ylim=range(c(sp1(),sp2())), axes = FALSE, xlab = "", ylab = "", col = 'blue', pch = 17, cex = 1.5)
   #  })
@@ -134,28 +134,28 @@ function(input, output) {
   #  })
   #  output$mean_diff <- renderText({
   #    mean(sp1()) - mean(sp2())
-      
+  
   #  })
-    
+  
 }
-  
-  
-  ##  mean_diff <- mean(hx1) - mean(hx2)
-  ##  zscore <- (mean(hx1) - mean(hx2))/(sqrt( ((sd1)^2)/(n1) + ((sd2)^2)/(n2) ))
-  ##pval <- round(t.test(hx1, hx2)$p.value, digits = 3)
-  ##data <- c(mean_diff, zscore, pval)
-  
-  
-  
-  # Q: overlay the two graphs or show in a column?
-  
 
-  #plot(hx1, col="red", type = "h", xlab = "", ylab = "",
-  #     main = "normal", axes = FALSE)
-  
-  #plot(hx2, col="green", type = "h", xlab = "", ylab = "",
-  #     main = "normal", axes = FALSE)
-  
+
+##  mean_diff <- mean(hx1) - mean(hx2)
+##  zscore <- (mean(hx1) - mean(hx2))/(sqrt( ((sd1)^2)/(n1) + ((sd2)^2)/(n2) ))
+##pval <- round(t.test(hx1, hx2)$p.value, digits = 3)
+##data <- c(mean_diff, zscore, pval)
+
+
+
+# Q: overlay the two graphs or show in a column?
+
+
+#plot(hx1, col="red", type = "h", xlab = "", ylab = "",
+#     main = "normal", axes = FALSE)
+
+#plot(hx2, col="green", type = "h", xlab = "", ylab = "",
+#     main = "normal", axes = FALSE)
+
 
 
 
--- Jinlin-Yuan/Visualizing_p_value/ui.R
+++ Jinlin-Yuan/Visualizing_p_value/ui.R
@@ -1,49 +1,49 @@
 fluidPage(
   headerPanel("Visualizing two-sample t-test"),
   sidebarLayout(  
-  sidebarPanel(
-    
+    sidebarPanel(
+      
       width = 3,
       
       selectInput(inputId='population1', 
                   label = 'population1',
                   choice = c('normal','skewed','uniform')
-                  ),
+      ),
       numericInput(inputId = 'popu_mean1',
-                label = 'Population mean', 
-                0,
-                min = -100, max = 100),
+                   label = 'Population mean', 
+                   0,
+                   min = -100, max = 100),
       
       numericInput(inputId = 'size1',
-                label = 'Sample size',
+                   label = 'Sample size',
                    20,
-                min = -100, max = 100),
+                   min = -100, max = 100),
       numericInput(inputId = 'sd1',
                    label = 'Standard diviation',
                    10,
-                min = -100, max = 100),
+                   min = -100, max = 100),
       
       selectInput(inputId='population2', 
                   label = 'population2',
                   choice = c('normal','skewed','uniform')
       ),
       numericInput(inputId = 'popu_mean2',
-                label = 'Population mean', 
-                0,
-                min = -100, max = 100),
+                   label = 'Population mean', 
+                   0,
+                   min = -100, max = 100),
       
       numericInput(inputId = 'size2',
-                label = 'Sample size',
-                20,
-                min = -100, max = 100),
+                   label = 'Sample size',
+                   20,
+                   min = -100, max = 100),
       numericInput(inputId = 'sd2',
-                label = 'Standard diviation',
-                10,
-                min = -100, max = 100),
+                   label = 'Standard diviation',
+                   10,
+                   min = -100, max = 100),
       selectInput(inputId='rep', 
-              label = 'Number of Samples',
-              choice = c(100,200,500,1000,2000,5000))),
-     
+                  label = 'Number of Samples',
+                  choice = c(100,200,500,1000,2000,5000))),
+    
     
     mainPanel(
       plotOutput("distPlot1"),
@@ -51,11 +51,11 @@ fluidPage(
       plotOutput("mean_diff1"),
       plotOutput("mean_diff2"),
       plotOutput("mean_diff3")
-   #   plotOutput("distPlot3"),
-     # verbatimTextOutput("test_stats"),
-  #   textOutput("test")
+      #   plotOutput("distPlot3"),
+      # verbatimTextOutput("test_stats"),
+      #   textOutput("test")
     )
-  
-    ))
+    
+  ))
 
 

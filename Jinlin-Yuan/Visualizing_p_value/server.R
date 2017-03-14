e0a3d44d3be2babe5d29406f09a485300490c1ca
wangyuan17sp <wangyuan@grinnell.edu>
2017-03-14 16:03:02 -0500

    udpate

--- Jinlin-Yuan/Visualizing_p_value/server.R
+++ Jinlin-Yuan/Visualizing_p_value/server.R
@@ -1,22 +1,22 @@
 function(input, output) {
   hx1 <- reactive({
     switch(input$population1,
-           "normal" = {rnorm(500, input$popu_mean1, input$sd1)}, 
-           "skewed" = {rchisq(500, df=4) + input$popu_mean1},
-           "uniform" = {runif(500, min = input$popu_mean1 - input$sd1, max = input$popu_mean1 + input$sd1)})
+           "normal" = {rnorm(5000, input$popu_mean1, input$sd1)}, 
+           "skewed" = {rchisq(5000, df=4) + input$popu_mean1},
+           "uniform" = {runif(5000, min = input$popu_mean1 - input$sd1, max = input$popu_mean1 + input$sd1)})
   })
   hx2 <- reactive({
     switch(input$population2,
-           "normal" = {rnorm(500, input$popu_mean2, input$sd2)},
-           "skewed" = {rchisq(500, df=4) + input$popu_mean2},
-           "uniform" = {runif(500, min = input$popu_mean2 - input$sd2, max = input$popu_mean2 + input$sd2)})
+           "normal" = {rnorm(5000, input$popu_mean2, input$sd2)},
+           "skewed" = {rchisq(5000, df=4) + input$popu_mean2},
+           "uniform" = {runif(5000, min = input$popu_mean2 - input$sd2, max = input$popu_mean2 + input$sd2)})
   })
   
   output$distPlot1 <- renderPlot({
     ## made change 1 for dynamic plotting
     low_range = min(input$popu_mean1-4*input$sd1, input$popu_mean2-4*input$sd2)
     high_range = max(input$popu_mean1+4*input$sd1, input$popu_mean2+4*input$sd2)
-    height1 = max(0.01*input$sd1, 0.01*input$sd2)
+    height1 = max(0.4/input$sd1, 0.4/input$sd2)
     height2 = max(0.1*input$sd1, 0.1*input$sd2)
     half_width =  max(4*input$sd2, 4*input$sd1)
     #height2 = 0.4/input$sd2
@@ -25,8 +25,8 @@ function(input, output) {
              curve(dnorm(x, mean = input$popu_mean1, sd = input$sd1),
                    xlim=c(low_range,high_range),
                    ylim = c(0,height1), 
-                   col="red",  lwd=2,  yaxt="n")
-             abline(v = 0)},
+                   col="red",  lwd=2,  yaxt="n",
+                   ylab="")},
            
            "skewed" = {
              curve(dchisq(x, df = 4), 
@@ -79,9 +79,10 @@ function(input, output) {
     data <- vector()
     data <- cbind(data, data())
     
-    hist(data[1,], col = "plum", pch = 16, xlab = "", main = "Histogram of Mean Difference", breaks = 30)
+    hist(data[1,], col = "plum", pch = 16, xlab = "", main = "Histogram of Mean Difference", breaks = 100)
     abline(v = input$popu_mean1 - input$popu_mean2, lwd = 2, col = "red")
-    plot(data[1,], seq_along(data[1,]), col = "plum", pch = 16, xlab = "", main = "Scatterplot of t-statistics")
+    
+    plot(data[1,], seq_along(data[1,]), col = "plum", pch = 16, xlab = "", main = "Scatterplot of Mean Difference")
     abline(v = input$popu_mean1 - input$popu_mean2, lwd = 2, col = "red")
   })
   
@@ -90,7 +91,7 @@ function(input, output) {
     data <- vector()
     data <- cbind(data, data())
     
-    hist(data[2,], col = "wheat1", pch = 16, xlab = "", main = "Histogram of t-statistics", breaks = 30)
+    hist(data[2,], col = "wheat1", pch = 16, xlab = "", main = "Histogram of t-statistics", breaks = 100)
     abline(v = (input$popu_mean1 - input$popu_mean2)/(sqrt(input$sd1 * input$sd1/input$size1 + input$sd2 * input$sd2/input$size2)), lwd = 2, col = "red")  
     plot(data[2,], seq_along(data[2,]), col = "wheat1", pch = 16, xlab = "", main = "Scatterplot of t-statistics")
     abline(v = (input$popu_mean1 - input$popu_mean2)/(sqrt(input$sd1 * input$sd1/input$size1 + input$sd2 * input$sd2/input$size2)), lwd = 2, col = "red")
@@ -101,10 +102,9 @@ function(input, output) {
     data <- vector()
     data <- cbind(data, data())
     
-    hist(data[3,], col= "palegreen", pch = 20, xlab = "", main = "Histogram of p-values", breaks = 30)
+    hist(data[3,], col= "palegreen", pch = 20, xlab = "", main = "Histogram of p-values", breaks = 100)
     abline(v = 0.05, lwd = 2, col = "red")
     plot(data[3,], seq_along(data[3,]), col= "palegreen", pch = 20, xlab = "", main = "Scatterplot of p-values")
     abline(v = 0.05, lwd = 2, col = "red")
   })
-  
 }

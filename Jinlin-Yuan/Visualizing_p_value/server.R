25ffb18331b951e065916bc5340cd23874c3b78f
wangyuan17sp <wangyuan@grinnell.edu>
2017-04-22 00:34:31 -0500

    final version of app1

--- Jinlin-Yuan/Visualizing_p_value/server.R
+++ Jinlin-Yuan/Visualizing_p_value/server.R
@@ -12,6 +12,20 @@ function(input, output) {
            "uniform" = {runif(5000, min = input$popu_mean2 - input$sd2, max = input$popu_mean2 + input$sd2)})
   })
   
+  std1 <- reactive({
+    switch(input$population1,
+           "normal" = {input$sd1},
+           "skewed" = {sqrt(input$sd1 * 2)},
+           "uniform" = {sqrt(1/12) * input$sd1 * 2})
+  })
+  std2 <- reactive({
+    switch(input$population2,
+           "normal" = {input$sd2},
+           "skewed" = {sqrt(input$sd2 * 2)},
+           "uniform" = {sqrt(1/12) * input$sd2 * 2})
+  })
+  
+  
   output$menu <- renderMenu({
     sidebarMenu(
       menuItem("Population 1", icon = NULL,
@@ -63,6 +77,20 @@ function(input, output) {
                               label = 'Std. deviation/df',
                               10,
                               min = -100, max = 100))
+      ),
+      sliderInput("slider", "Number of trials:", 20, 5000, 1000),
+      actionButton("action", "Run"),
+      menuItem("Preferences", icon = NULL,
+               menuSubItem(
+                 icon = NULL,
+                 sliderInput("slider1", "Number of breaks:", 5, 150, 50)
+               ),
+               menuSubItem(
+                 icon = NULL,
+                 numericInput("alpha", "Alpha Level",
+                              0.05,
+                              min = 0, max = 1)
+               )
       ))
   })
   
@@ -122,15 +150,17 @@ function(input, output) {
            })
   })
   
-  data <- reactive({
+  data <- eventReactive(input$action, {
     d <- vector()
     for (i in 1:input$slider) {
       sp1 <- sample(hx1(), input$size1)
       sp2 <- sample(hx2(), input$size2)
       mean_diff <- round(mean(sp1) - mean(sp2), digits = 5)
-      ttest <- t.test(sp1, sp2, paired = FALSE)
+      ttest <- t.test(sp1, sp2, paired = FALSE, conf.level = 1 - input$alpha)
       tscore <- round(ttest$statistic, digits = 5)
       pval <- round(ttest$p.value, digits = 5)
+      confidence_l <- round((ttest$conf)[1])
+      confidence_l <- round((ttest$conf)[1])
       d <- cbind(d, c(mean_diff, tscore, pval))
     }
     return(d)
@@ -149,28 +179,51 @@ function(input, output) {
     ggplot(h, aes(x = title, fill = pop)) + geom_dotplot(binwidth=1, alpha = 0.7) + ylim(0, height1)
   })
   
+  t_dist <- reactive({
+    qt(c(input$alpha / 2, 1 - input$alpha / 2), df = input$size1 + input$size2 - 2)
+  })
+  
   output$mean_diff1 <- renderPlot({
     data <- vector()
     data <- cbind(data, data())
+    t_dist <- t_dist()
+    sd1 <- std1()
+    sd2 <- std2()
     
-    hist(data[1,], col = "plum", pch = 16, xlab = "", ylab = "", main = NULL, breaks = input$slider1)
-    abline(v = input$popu_mean1 - input$popu_mean2, lwd = 2, col = "red")
+    denominator <- sqrt(sd1*sd1/input$size1 + sd2*sd2/input$size2)
+    
+    h <- hist(data[1,], col = "plum", pch = 16, 
+              breaks = input$slider1,
+              plot = FALSE)
+    cuts <- cut(h$breaks, c(-Inf, t_dist[1] * denominator, t_dist[2] * denominator, Inf))
+    plot(h, xlab = "", ylab = "", main = paste("Mean Difference = ", input$popu_mean1 - input$popu_mean2),
+         col = c("red", "plum", "red")[cuts])
+    #abline(v = input$popu_mean1 - input$popu_mean2, lwd = 2, col = "red")
     
   })
   
   output$mean_diff2 <- renderPlot({
     data <- vector()
     data <- cbind(data, data())
+    t_dist <- t_dist()
     
-    hist(data[2,], col = "wheat1", pch = 16, xlab = "", ylab = "", main = NULL, breaks = input$slider2)
-    abline(v = (input$popu_mean1 - input$popu_mean2)/(sqrt(input$sd1 * input$sd1/input$size1 + input$sd2 * input$sd2/input$size2)), lwd = 2, col = "red")  
+    h <- hist(data[2,], col = "wheat1", pch = 16, 
+              breaks = input$slider1, plot = FALSE) 
+    cuts <- cut(h$breaks, c(-Inf, t_dist[1], t_dist[2], Inf))
+    plot(h, xlab = "", ylab = "", main = NULL, 
+         col = c("red", "wheat1", "red")[cuts])
+    #abline(v = c(t_dist[1], t_dist[2]), lwd = c(2, 2), col = c("red", "red"))
   })
   
   output$mean_diff3 <- renderPlot({
     data <- vector()
     data <- cbind(data, data())
     
-    hist(data[3,], col= "palegreen", pch = 20, xlab = "", ylab = "", main = NULL, breaks = input$slider3)
-    abline(v = 0.05, lwd = 2, col = "red")
+    h <- hist(data[3,], col= "palegreen", pch = 20, 
+              breaks = input$slider1, plot = FALSE)
+    cuts <- cut(h$breaks, c(-Inf, input$alpha, Inf))
+    plot(h, xlab = "", ylab = "", main = paste("alpha = ", input$alpha), 
+         col = c("red", "palegreen")[cuts])
+    #abline(v = input$alpha, lwd = 2, col = "red")
   })
 }
--- Jinlin-Yuan/Visualizing_p_value/ui.R
+++ Jinlin-Yuan/Visualizing_p_value/ui.R
@@ -3,69 +3,53 @@ library(ggplot2)
 
 dashboardPage(
   dashboardHeader(title = "Visualizing two-sample t-test", titleWidth = 280),
-  dashboardSidebar(  
+  dashboardSidebar(
     sidebarMenuOutput("menu"),
-    sliderInput("slider", "Number of trials:", 20, 5000, 1000),
     htmlOutput("text")
-    ),
+  ),
   
   
   dashboardBody(
     fluidRow(
       box(
-        title = "Population Distribution", 
+        title = "Populations Distribution", 
         width = 6,
         status = "primary",
         plotOutput("distPlot1", height = 300)
       ),
       
       box(
-        title = "Sample Distribution", 
+        title = "Samples Distribution", 
         width = 6,
         status = "primary",
         plotOutput("sample_dist", height = 300)
       )),
     
     fluidRow(
-      column(width = 4,
-             box(
-               title = "Histogram of Mean Difference",
-               width = NULL,
-               status = "primary",
-               plotOutput("mean_diff1", height = 250)
-             ),
-             box(
-               title = "Number of breaks",
-               width = NULL,
-               sliderInput("slider1", "Number of breaks:", 5, 200, 100)
-             )),
+      box(
+        title = "Histogram of Mean Differences",
+        width = 4,
+        status = "primary",
+        plotOutput("mean_diff1", height = 250)
+      ),
       
-      column(width = 4,
-             box(
-               title = "Histogram of t-statistics",
-               width = NULL,
-               status = "primary",
-               plotOutput("mean_diff2", height = 250)
-             ),
-             box(
-               title = "Number of breaks",
-               width = NULL,
-               sliderInput("slider2", "Number of breaks:", 5, 200, 100)
-             )),
+      box(
+        title = "Histogram of t-statistics",
+        width = 4,
+        status = "primary",
+        plotOutput("mean_diff2", height = 250)
+      ),
       
-      column(width = 4,
-             box(
-               title = "Histogram of p-values",
-               width = NULL,
-               status = "primary",
-               plotOutput("mean_diff3", height = 250)
-             ),
-             box(
-               title = "Number of breaks",
-               width = NULL,
-               sliderInput("slider3", "Number of breaks:", 5, 200, 100)
-             ))
-    )))
+      box(
+        title = "Histogram of p-values",
+        width = 4,
+        #width = NULL,
+        status = "primary",
+        plotOutput("mean_diff3", height = 250)
+      )
+    )
+  )
+)
 
 
 
